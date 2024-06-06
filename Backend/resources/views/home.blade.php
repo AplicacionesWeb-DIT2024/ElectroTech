40cd1dc877baa65dd65018fca3ccd585687b6cf4
TomasNRA <tom007_ra@hotmail.com>
2024-06-06 20:49:09 -0300

    Autentificacion back

---
+++ Backend/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+@section('icon')
+<i class="fa-solid fa-house"></i> 
+@endsection
+@section('title')
+Home
+@endsection
--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -3,10 +3,25 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\LoginController;
 
-Route::get('/', function () {
+Route::get('welcome', function () {
     return view('welcome');
 });
 
-Route::resource('categorias', CategoriaController::class);
-Route::resource('productos', ProductoController::class);
+Route::middleware('auth')->group(function () {
+    Route::view('home', 'home')->name('home');
+    Route::resource('categorias', CategoriaController::class);
+    Route::resource('productos', ProductoController::class);
+    Route::get('logout',[LoginController::class, 'logout'])->name('logout');
+    Route::get('logout',[LoginController::class, 'logout'])->name('logout');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::view('/', 'login')->name('login');
+    Route::post('inicio-sesion',[LoginController::class, 'login'])->name('inicio-sesion');
+});
+
+
+
+
