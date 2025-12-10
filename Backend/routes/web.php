f5bd08465038738859dbe37a9994a7b068ce4494
TomasNRA <tom007_ra@hotmail.com>
2025-12-10 20:19:26 -0300

    Test

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\NewsController;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 
 Route::get('welcome', function () {
     return view('welcome');
@@ -26,4 +27,29 @@ Route::middleware('guest')->group(function () {
 
 
 
+Route::get('/test-cloudinary', function () {
+    try {
+        // Subimos un archivo temporal muy pequeño
+        $tempFile = tmpfile();
+        $meta = stream_get_meta_data($tempFile);
+        $tmpFilePath = $meta['uri'];
 
+        file_put_contents($tmpFilePath, 'test'); // contenido mínimo
+
+        $uploadedFile = Cloudinary::upload($tmpFilePath, ['folder' => 'test']);
+
+        // Obtenemos URL segura
+        $url = $uploadedFile->getSecurePath();
+
+        fclose($tempFile); // cerramos el temp file
+        return response()->json([
+            'success' => true,
+            'url' => $url
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'success' => false,
+            'error' => $e->getMessage()
+        ]);
+    }
+});
